ca7586120c91c39b19343d1804da66fa1f4ee430
jerry99414-cloud <jerry99414@gmail.com>
2026-06-29 13:36:32 +0800

    1

--- all-source-files/artifacts/api-server/src/app.ts
+++ all-source-files/artifacts/api-server/src/app.ts
@@ -1,10 +1,20 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import path from "path";
+import { fileURLToPath } from "url";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const frontendDistPath = path.resolve(
+  __dirname,
+  "../../inspection-schedule/dist/public",
+);
 
 app.use(
   pinoHttp({
@@ -25,10 +35,17 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
+
+app.use(express.static(frontendDistPath));
+
+app.get(/.*/, (_req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
+});
 
 export default app;
